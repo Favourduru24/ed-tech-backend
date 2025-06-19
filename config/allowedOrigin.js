5f88625f89106dc730978e47bebf6783698841b2
unknown <Favourduru24@gmail.com>
2025-06-18 19:27:22 -0700

    placing frontend deployed url link

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,8 +1,6 @@
 const allowedOrigin = [
     "http://localhost:3000",
-    "http://localhost:4000/feeds",
-    "http://localhost:5173",
-    "https://www.google.com"
-  ];
+    "https://ed-tech-frontend-e8zm.vercel.app"
+    ];
 
 module.exports = allowedOrigin
