a470444fb61635cedb1ba5914403899e43cbf829
Duru Favour <favourduru24@gmail.com>
2026-01-01 01:03:33 -0800

    new code update redeploying

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,14 +1,15 @@
-const allowedOrigin = require('./allowedOrigin')
+const allowedOrigins = require('./allowedOrigin');
 
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if(allowedOrigin.indexOf(origin) !== -1 || !origin){
-        callback(null, true) 
-          } else{
-            callback(new Error ('Not allowed by Cors'))
-          }
-      }, 
-      optionSuccessStatus:200
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
+  },
+  credentials: true,          
+  optionsSuccessStatus: 200 
+};
 
 module.exports = corsOptions;
